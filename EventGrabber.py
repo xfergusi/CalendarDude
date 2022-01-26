c7c373d27794c2d51da9004c75d6297de7189b74
Ian Ferguson <xfergusi@gmail.com>
2022-01-27 09:28:31 +1100

    breaking into its own class, I think I need to use it a bit

--- EventGrabber.py
+++ EventGrabber.py
@@ -10,35 +10,16 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
+from AuthorizationSteve import AuthorizationSteve
+
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
 class EventGrabber:
     @staticmethod
     def get_events():
-        """Shows basic usage of the Google Calendar API.
-        Prints the start and name of the next 10 events on the user's calendar.
-        """
-        creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
-
         try:
-            service = build('calendar', 'v3', credentials=creds)
+            service = build('calendar', 'v3', credentials=AuthorizationSteve.check_creds())
 
             # page_token = None
             # while True:
