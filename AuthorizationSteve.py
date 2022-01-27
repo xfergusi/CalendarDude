67f8d74ec41322129de5ef32062ba817076ed9fe
Ian Ferguson <xfergusi@gmail.com>
2022-01-27 12:02:00 +1100

    misc files and hey look, i made an event

--- AuthorizationSteve.py
+++ AuthorizationSteve.py
@@ -6,7 +6,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
 
 class AuthorizationSteve:
--- main.py
+++ main.py
@@ -1,42 +1,27 @@
 from AuthorizationSteve import AuthorizationSteve
+import pytz
 from EventGrabber import EventGrabber
 
 
 def main():
+    print(pytz.all_timezones)
+    service = AuthorizationSteve().get_service()
+    event = {
+        'summary': 'test',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2022-01-28T09:00:00-07:00',
+            'timeZone': 'Australia/NSW',
+        },
+        'end': {
+            'dateTime': '2022-01-28T17:00:00-07:00',
+            'timeZone': 'Australia/NSW',
+        },
+    }
 
-    # service = AuthorizationSteve.get_service();
-    # event = {
-    #     'summary': 'Google I/O 2015',
-    #     'location': '800 Howard St., San Francisco, CA 94103',
-    #     'description': 'A chance to hear more about Google\'s developer products.',
-    #     'start': {
-    #         'dateTime': '2015-05-28T09:00:00-07:00',
-    #         'timeZone': 'America/Los_Angeles',
-    #     },
-    #     'end': {
-    #         'dateTime': '2015-05-28T17:00:00-07:00',
-    #         'timeZone': 'America/Los_Angeles',
-    #     },
-    #     'recurrence': [
-    #         'RRULE:FREQ=DAILY;COUNT=2'
-    #     ],
-    #     'attendees': [
-    #         {'email': 'lpage@example.com'},
-    #         {'email': 'sbrin@example.com'},
-    #     ],
-    #     'reminders': {
-    #         'useDefault': False,
-    #         'overrides': [
-    #             {'method': 'email', 'minutes': 24 * 60},
-    #             {'method': 'popup', 'minutes': 10},
-    #         ],
-    #     },
-    # }
-    #
-    # event = service.events().insert(calendarId='primary', body=event).execute()
-    # print('Event created: %s' % (event.get('htmlLink')))
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
 
-    EventGrabber.get_events()
 
 if __name__ == '__main__':
     main()
