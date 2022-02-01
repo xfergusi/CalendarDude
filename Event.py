d1dcff454d0fff1ae132ffb00cef39f91a1bc4c2
Ian Ferguson <xfergusi@gmail.com>
2022-02-01 13:55:29 +1100

    cleaner, a bit

--- Event.py
+++ Event.py
@@ -12,8 +12,8 @@ class Event:
         self.event_id = event_id
         self.summary = summary
         self.description = event_id
-        self.start = start
-        self.end = end
+        self.start = str(start)
+        self.end = str(end)
 
     def make_an_all_day_event(self):
         EventCreator().make_an_all_day_event(self.summary, self.description, self.start, self.end)
--- EventCreator.py
+++ EventCreator.py
@@ -5,24 +5,21 @@ from AuthorizationSteve import AuthorizationSteve
 
 class EventCreator:
     def __make_an_event(self, summary, description, start, end, allday):
-        print(summary)
-        print(description)
-        print(str(start)[0:10])
-        print(end)
-        print(allday)
         time_period = "dateTime"
         if allday:
             time_period = "date"
+            start = start[0:10]
+            end = end[0:10]
         service = AuthorizationSteve().get_service()
         event = {
             'summary': summary,
             'description': description,
             'start': {
-                time_period: str(start)[0:10],
+                time_period: start,
                 'timeZone': 'Australia/Sydney',
             },
             'end': {
-                time_period: str(end)[0:10],
+                time_period: end,
                 'timeZone': 'Australia/Sydney',
             },
         }
